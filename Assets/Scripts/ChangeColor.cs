a888bc4c174269760a3c73d5f3721b540af2294f
Omoto <ractimpact@gmail.com>
2019-04-30 20:41:33 +0900

    Update：シーンの更新　Add：色を変更するスクリプト（仮）

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColorEvent()
+    {
+        if (image.color == Color.white)
+        {
+            image.color = Color.gray;
+        }
+        else
+        {
+            image.color = Color.white;
+        }
+    }
+}
