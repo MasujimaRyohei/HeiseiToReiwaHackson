a1079dce5f6537f5651673e2a0bf8b117bb674d6
MasujimaRyohei <nushibeatbox@gmail.com>
2019-04-30 18:03:23 +0900

    Add：Excelファイル読み込み機能

---
+++ Assets/MasterData/CardsData.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+///
+/// !!! Machine generated code !!!
+/// !!! DO NOT CHANGE Tabs to Spaces !!!
+/// 
+[System.Serializable]
+public class CardsData
+{
+  [SerializeField]
+  int id;
+  public int Id { get {return id; } set { id = value;} }
+  
+  [SerializeField]
+  string name;
+  public string Name { get {return name; } set { name = value;} }
+  
+  [SerializeField]
+  int value;
+  public int Value { get {return value; } set { value = value;} }
+  
+  [SerializeField]
+  int price;
+  public int Price { get {return price; } set { price = value;} }
+  
+  [SerializeField]
+  string description;
+  public string Description { get {return description; } set { description = value;} }
+  
+  [SerializeField]
+  string comment;
+  public string Comment { get {return comment; } set { comment = value;} }
+  
+}
