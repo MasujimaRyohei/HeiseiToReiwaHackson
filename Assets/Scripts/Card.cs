f2b60f265661b6f90f11502ac68ed91236690d87
MasujimaRyohei <nushibeatbox@gmail.com>
2019-05-01 12:42:54 +0900

    Add：カードプレハブ

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class Card: MonoBehaviour
+{
+    public CardsData data;
+    public Image image;
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        string path = "Images/Cards/Card" + data.Id;
+        Sprite sprite = Resources.Load(path,typeof(Sprite)) as Sprite;
+        print(sprite);
+        image.sprite = sprite;
+        text.text = data.Description;
+    }
+}
